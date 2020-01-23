90a475522473b4f32b2a927b81f317b5f141dad0
Tzvi-H <16guitar@gmail.com>
2020-01-23 11:41:35 +0800

    initial completion of protein-translation

---
+++ easy/protein-translation.js
@@ -0,0 +1,36 @@
+const CODON_TO_PROTEIN = {
+  'AUG': 'Methionine',
+  'UUU': 'Phenylalanine', 
+  'UUC': 'Phenylalanine',
+  'UUA': 'Leucine',
+  'UUG': 'Leucine',
+  'UCU': 'Serine',
+  'UCC': 'Serine',
+  'UCA': 'Serine',
+  'UCG': 'Serine',
+  'UAU': 'Tyrosine',
+  'UAC': 'Tyrosine',
+  'UGU': 'Cysteine',
+  'UGC': 'Cysteine',
+  'UGG': 'Tryptophan',
+  'UAA': 'STOP',
+  'UAG': 'STOP',
+  'UGA': 'STOP'	
+}
+
+function translate(rna = '') {
+  let proteins = [];
+
+  for (let index = 0; index < rna.length; index += 3) {
+    let protein = CODON_TO_PROTEIN[rna.slice(index, index + 3)];
+
+    if (protein === 'STOP') break;
+    if (protein === undefined) throw new Error('Invalid codon');
+
+    proteins.push(protein);
+  }
+
+  return proteins;
+}
+
+module.exports = translate;
